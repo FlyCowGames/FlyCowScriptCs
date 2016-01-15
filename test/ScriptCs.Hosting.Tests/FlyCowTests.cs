6e87012cfbcd107e19160740678b1c44944f6338
Vladimir Bodurov <vladimir.bodurov@flycowgames.com>
2016-01-15 13:03:53 -0800

    tests

--- test/ScriptCs.Hosting.Tests/FlyCowTests.cs
+++ test/ScriptCs.Hosting.Tests/FlyCowTests.cs
@@ -10,7 +10,7 @@ namespace ScriptCs.Hosting.Tests
         public void EncryptPassword()
         {
             IEncryptionProvider cp = new EncryptionProvider(new DataEncoder());
-            var p = cp.Encript("password", DataUtil.HexStringToBytes("0x7B2566EFE30E11CCAC515B0D256AE25FC5D048D6A199CCD8A0640604744DB66123B7B150E626D3EC4E508A0565532F7B5446BE8E82DCDB3D0BD0864FA4CCD5EA"));
+            var p = cp.Encript("fly99cow!", DataUtil.HexStringToBytes("0x7B2566EFE30E11CCAC515B0D256AE25FC5D048D6A199CCD8A0640604744DB66123B7B150E626D3EC4E508A0565532F7B5446BE8E82DCDB3D0BD0864FA4CCD5EA"));
             Console.WriteLine(p);
         }
     }
